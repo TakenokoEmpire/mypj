8e924c0cf0084be3a3d419824b105c68924aab34
TakenokoEmpire <j2fi9x@gmail.com>
2021-09-21 17:22:02 +0900

    test by checking answer counter

--- tests/test_mypj.py
+++ tests/test_mypj.py
@@ -1,5 +1,14 @@
+import pytest
 from mypj.mypj import main
+from mypj.number_guess import NumberGuess
 
 
-def test_main():
-    assert 1 == 1
+def test_main() -> None:
+    """
+    main()関数のテスト
+    分割統治法で解いたときの回数をチェックする
+    """
+    runner = NumberGuess(min_ans=0, max_ans=99, max_stage=100)
+    stage, history = runner.run("binary")
+
+    # assert stage == 5
