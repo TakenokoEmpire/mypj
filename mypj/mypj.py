4eccfd5352789e508484654500dfea80d5dd712a
TakenokoEmpire <j2fi9x@gmail.com>
2021-09-21 17:22:45 +0900

    main関数とパーサーの追加

--- mypj/mypj.py
+++ mypj/mypj.py
@@ -1,10 +1,41 @@
-def main():
-    a = [1, 3, 4, 5]
-    b = {"name": "flower", "color": "red"}
+# coding: UTF-8
+"""(ry
 
-    print(":::::")
+"""
+import argparse
+from . import number_guess
 
 
-# 直接入力したときだけ適用できるようにするためのif文（importではできない）
-if __name__ == "__main__":
-    main()
+def get_parser() -> argparse.Namespace:
+    """コマンドライン引数を解析したものを持つ
+    """
+
+    parser = argparse.ArgumentParser(description="数当てゲーム")
+
+    parser.add_argument('--min_ans', default=0)
+    parser.add_argument('--max_ans', default=99)
+    parser.add_argument('--max_stage', default=100)
+    parser.add_argument('--ans', default=None)
+    parser.add_argument('--mode', default="manual")
+
+    args = parser.parse_args()
+    return args
+
+
+def main() -> None:
+    """数当てゲームのメイン
+    """
+    args = get_parser()
+    min_ans = int(args.min_ans)
+    max_ans = int(args.max_ans)
+    max_stage = int(args.max_stage)
+    mode = args.mode
+
+    if args.ans is not None:
+        ans = int(args.ans)
+        runner = number_guess.NumberGuess(
+            min_ans=min_ans, max_ans=max_ans, max_stage=max_stage, ans=ans)
+    else:
+        runner = number_guess.NumberGuess(
+            min_ans=min_ans, max_ans=max_ans, max_stage=max_stage)
+    stage, history = runner.run(mode=mode)
